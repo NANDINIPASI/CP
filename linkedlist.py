fe7cffddceb6d0475299de2071c3bbaaf202d2a8
Nandini <145583472+NANDINIPASI@users.noreply.github.com>
2024-03-16 10:46:22 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
          prev  = ListNode(0)
